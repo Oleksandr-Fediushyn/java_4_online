09da58c1a3c9f7f9ff6bd44e909985d36dfabe73
Oleksandr-Fediushyn <docent77rks@gmail.com>
2023-05-31 00:12:27 +0300

    add module_3

--- my_web_project/src/main/java/ua/com/alevel/SoftwareCatalogApplication.java
+++ my_web_project/src/main/java/ua/com/alevel/SoftwareCatalogApplication.java
@@ -1,16 +1,8 @@
 package ua.com.alevel;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import ua.com.alevel.persistence.entity.user.Admin;
-import ua.com.alevel.persistence.entity.user.Manager;
-import ua.com.alevel.persistence.repository.user.AdminRepository;
-import ua.com.alevel.persistence.repository.user.ManagerRepository;
+
 
 @SpringBootApplication
 public class SoftwareCatalogApplication {
